7739bfc4371ee13053905925e3fb7bca68788281
haubaustin <haubaustin@gmail.com>
2022-03-27 11:08:59 -0700

    Fixed Invalid Hooks not rendering React

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 
 
 const Nav = () => {
     return (
         <div className='Nav'>
-            <Link to="/">Home</Link>
-            <Link to="/about">About</Link>
+            {/* <Link to="/">Home</Link>
+            <Link to="/about">About</Link> */}
             <span className='title'>
                 Cert Expert
             </span>
